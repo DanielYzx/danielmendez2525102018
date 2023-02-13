891ef005af3c801a12409eaa887b9ea10bcd124d
DanielMéndez <2525102018@mail.utec.edu.sv>
2023-02-12 18:42:17 -0600

    0602202319:37

---
+++ Contrrolesu/app/src/main/java/utec/edu/sv/contrrolesu/MainActivity.java
@@ -0,0 +1,21 @@
+package utec.edu.sv.contrrolesu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtValor, edtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtValor=findViewById(R.id.edtTabla);
+        edtResultado=findViewById(R.id.edtMtabla);
+
+    }
+}
