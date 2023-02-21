35e480ae0de1e523f66b57328646de308a78e7a2
DanielMéndez <2525102018@mail.utec.edu.sv>
2023-02-21 13:50:17 -0600

    9090

---
+++ ProyectoListenner/app/src/main/java/utec/edu/sv/proyectolistenner/Principal.java
@@ -0,0 +1,40 @@
+package utec.edu.sv.proyectolistenner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Principal extends AppCompatActivity {
+Button btnMensaje;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal2);
+        btnMensaje=findViewById(R.id.btnToast);
+        btnMensaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toastP = new Toast(getApplicationContext());
+                LayoutInflater inflater= getLayoutInflater();
+                View layout=inflater.inflate(R.layout.toast_layout,(ViewGroup) findViewById(R.id.lytMensaje));
+
+                TextView txtMensaje=(TextView) layout.findViewById(R.id.textView3);
+                txtMensaje.setText("Soy mensaje");
+                toastP.setDuration(Toast.LENGTH_LONG);
+                toastP.setView(layout);
+                toastP.show();
+
+
+
+
+            }
+        });
+
+    }
+}
